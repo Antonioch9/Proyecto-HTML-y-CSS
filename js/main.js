60ded8103916f2725887adcdfffe1c4dac5864d9
Antonioch9 <acallejah1994@gmail.com>
2018-02-16 12:55:14 +0100

    añadimos los cambios para menu desplegable

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
     $(".cerrar").click(function(){
         $(".menu").animate({width:"toggle"},500);
     });
+    // menu haburguesa pagina normales
+    $(".burger2").click(function(){
+        $(".burger2").toggleClass("abierto");
+        $(".menu2").animate({width:"toggle"},500);
+        $(".menu2 a").toggleClass("aparicion");
+    });
+    
 });
